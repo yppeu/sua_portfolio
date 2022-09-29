d6480da830f6ae81dd06489321e64f4334d5636f
selly <88474556+yppeu@users.noreply.github.com>
2022-09-29 11:58:25 +0900

    Add files via upload

--- index.js
+++ index.js
@@ -3,48 +3,23 @@
 window.onload = function () {
 };
 
-/*    let content = 'SUA IS DEVELOPER.';
-    let contenttext = document.getElementById('maintext');
-    let i = 0;
 
-    function typing() {
-        if (i < content.length) {
-            let txt = content.charAt(i);
-            contenttext.innerHTML += txt;
-            console.log(contenttext);
-            i++;
-        }
-    }
-    setInterval(typing, 200);*/
+// setInterval(typing, 200);
+
+// $.ajax({
+//     type: "GET",
+//     url: "http://spartacodingclub.shop/sparta_api/weather/seoul",
+//     data: {},
+//     success: function (response) {
+//         let temps = response['temp']
+//         let cloud = response['clouds']
+
+//         $('#temp').append('현재 서울시 기온 : ' + temps + '˚' + `<br>` + '강수 확률 : ' + cloud);
+//         $('#temp').addClass('text-center');
+//     }
+// });
 
 
-let content = 'SUA IS DEVELOPER.';
-let contenttext = document.getElementById('maintext');
-let i = 0;
-
-function typing() {
-    let txt = content[i++];
-    contenttext.innerHTML += txt;
-    if (i > content.length) {
-        contenttext.textContent = '';
-        i = 0;
-    }
-}
-
-setInterval(typing, 200);
-
-$.ajax({
-    type: "GET",
-    url: "http://spartacodingclub.shop/sparta_api/weather/seoul",
-    data: {},
-    success: function (response) {
-        let temps = response['temp']
-        let cloud = response['clouds']
-
-        $('#temp').append('현재 서울시 기온 : ' + temps + '˚' + `<br>` + '강수 확률 : ' + cloud);
-        $('#temp').addClass('text-center');
-    }
-});
 
 /*
     function chrbtn() {
